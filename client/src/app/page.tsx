d741124bf5affaf6bc3b0a821d947e1446c8fa75
jitendragangwar123 <gangwarjitendra2498@gmail.com>
2024-07-15 00:23:39 +0530

    fix:page navigation link

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -74,7 +74,7 @@ export default function Home() {
             </p>
           </a>
           <a
-            href="/portfolio"
+            href="/purchase-assets"
             className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
             target="_blank"
             rel="noopener noreferrer"
@@ -105,7 +105,7 @@ export default function Home() {
           </a>
 
           <a
-            href="/portfolio"
+            href="/purchase-assets"
             className="group rounded-lg border border-transparent px-3 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
             target="_blank"
             rel="noopener noreferrer"
